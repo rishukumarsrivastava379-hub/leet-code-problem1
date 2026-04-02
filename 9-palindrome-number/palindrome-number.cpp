f7c3c67ce025c4f6716de74a9b5af7e9b92d606e
rishukumarsrivastava379-hub <rishukumarsrivastava379@gmail.com>
2026-04-02 09:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.31%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+
+        long rev = 0;
+        int temp = x;
+
+        while(temp != 0){
+            int digit = temp % 10;
+            rev = rev * 10 + digit;
+            temp = temp / 10;
+        }
+
+        return rev == x;
+    }
+};
