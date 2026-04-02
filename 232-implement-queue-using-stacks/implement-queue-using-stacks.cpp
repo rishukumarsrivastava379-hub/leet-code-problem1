2d16089f723eca3c9ad64efc9de3b2fdbb4fe4ad
rishukumarsrivastava379-hub <rishukumarsrivastava379@gmail.com>
2026-04-02 22:30:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (69.51%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -0,0 +1,38 @@
+class MyQueue {
+public:
+    stack<int> inputStack;
+    stack<int> outputStack;
+
+    MyQueue() {
+    }
+
+    void push(int x) {
+        inputStack.push(x);
+    }
+
+    int pop() {
+        if (outputStack.empty()) {
+            while (!inputStack.empty()) {
+                outputStack.push(inputStack.top());
+                inputStack.pop();
+            }
+        }
+        int front = outputStack.top();
+        outputStack.pop();
+        return front;
+    }
+
+    int peek() {
+        if (outputStack.empty()) {
+            while (!inputStack.empty()) {
+                outputStack.push(inputStack.top());
+                inputStack.pop();
+            }
+        }
+        return outputStack.top();
+    }
+
+    bool empty() {
+        return inputStack.empty() && outputStack.empty();
+    }
+};
