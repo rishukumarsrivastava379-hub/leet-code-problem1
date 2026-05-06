f08e022b0db75583747c429a16126438da481e58
rishukumarsrivastava379-hub <rishukumarsrivastava379@gmail.com>
2026-05-06 16:06:40 +0530

    Time: 11 ms (91.08%) | Memory: 25.1 MB (66.34%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end()); // sort characters
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+
+        for (auto &p : mp) {
+            result.push_back(p.second);
+        }
+
+        return result;
+    }
+};
